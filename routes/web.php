a10219a33ae15719457b88b45ba5771b030c94d6
Yogi Pratama <ypt@dharmap.com>
2026-05-26 18:05:51 +0700

    First Project

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,8 @@ Route::get('/', function () {
 });
 
 Route::middleware(['auth'])->group(function () {
-    Route::get('/dashboard', [DashboardController::class, 'index
+    Route::get('/dashboard', [DashboardController::class, 'index'])
+        ->name('dashboard');
 
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
